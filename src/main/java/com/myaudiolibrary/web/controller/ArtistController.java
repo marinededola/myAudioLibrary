2b2de86c9b7195137090ebbe9b61edd202d131df
marine <dedola.marine@hotmail.com>
2020-12-21 17:22:56 +0100

    Controllers et affichage details

---
+++ src/main/java/com/myaudiolibrary/web/controller/ArtistController.java
@@ -0,0 +1,75 @@
+package com.myaudiolibrary.web.controller;
+
+import com.myaudiolibrary.web.model.Artist;
+import com.myaudiolibrary.web.repository.ArtistRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.List;
+import java.util.Optional;
+
+
+@Controller
+@RequestMapping("/artists")
+public class ArtistController {
+
+    @Autowired
+    private ArtistRepository artistRepository;
+
+
+    /**
+     *
+     * @param id
+     * @param model
+     * @return les informations de l'artiste de l'id selectionné
+     */
+    @RequestMapping(method = RequestMethod.GET, value = "/{id}")
+    public String findArtistById(@PathVariable Integer id, final ModelMap model) {
+        Optional<Artist> artistOptionnal = artistRepository.findById(id);
+
+        if (artistOptionnal.isEmpty()) {
+            throw new EntityNotFoundException("L'artiste avec l'identifiant " + id + " n'a pas été trouvé");
+        }
+
+        model.put("artist", artistOptionnal.get());
+
+        return "detailArtist";
+    }
+
+    /**
+     *
+     * @param name
+     * @param model
+     * @return Liste des artistes avec le nom recherché
+     */
+    @RequestMapping(method = RequestMethod.GET, value = "", params = "matricule")
+    public String findArtistByName(@RequestParam("name") String name, final ModelMap model){
+        List<Artist> artists = artistRepository.findByName(name);
+        model.put("artists", artists);
+        return "listArtists";
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "")
+    public String listArtists(final ModelMap model,
+                               @RequestParam(defaultValue = "0") Integer page,
+                               @RequestParam(defaultValue = "10") Integer size,
+                               @RequestParam(defaultValue = "ASC") String sortDirection,
+                               @RequestParam(defaultValue = "name") String sortProperty){
+        PageRequest pageRequest = PageRequest.of(page, size,
+                Sort.Direction.fromString(sortDirection), sortProperty);
+        Page<Artist> pageArtists = artistRepository.findAll(pageRequest);
+        model.put("artists", pageArtists);
+        model.put("pageNumber", page + 1);
+        model.put("previousPage", page - 1);
+        model.put("nextPage", page + 1);
+        return "listArtists";
+    }
+}
